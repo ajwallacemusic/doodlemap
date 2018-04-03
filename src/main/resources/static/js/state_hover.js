ba09a568c0f2b9031c631c1f78b4b4697391c27b
AJ Wallace <ajwallacemusic@gmail.com>
2018-04-03 15:52:43 -0500

    mobile touch bug fix

--- src/main/resources/static/js/state_hover.js
+++ src/main/resources/static/js/state_hover.js
@@ -9,4 +9,14 @@ $("path, circle").mouseleave(function(e) {
 $(document).mousemove(function(e) {
   $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
   $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
-}).mouseover();
+});
+/*.mouseover() (was before semicolon above)*/
+
+/* maybe fix for mobile touch?
+
+$('#us-map').on('touchmove',function(e){
+      e.preventDefault();
+});
+
+*/
+
