39380791edd6ec480cd400edc76d4d8ee561c70e
AJ Wallace <ajwallacemusic@gmail.com>
2018-04-03 17:57:39 -0500

    mobile touch bug fixed

--- src/main/resources/static/js/state_hover.js
+++ src/main/resources/static/js/state_hover.js
@@ -1,12 +1,21 @@
+$(document).ready(function() {
+  $("a").on("click touchend", function(e) {
+    var el = $(this);
+    var link = el.attr("href");
+    window.location = link;
+  });
+});
 
-$("path, circle").hover(function(e) {
+
+$("path, circle").mouseover(function(e) {
   $('#info-box').css('display','block');
   $('#info-box').html('<div>' + $(this).data('info') + '</div>');
 });
+
 $("path, circle").mouseleave(function(e) {
   $('#info-box').css('display','none');
 });
-$(document).mousemove(function(e) {
+$("#us-map").mousemove(function(e) {
   $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
   $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
 });
